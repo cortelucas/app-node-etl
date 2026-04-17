d9f0ffc0d1fac96eb7be692d999a31f292abffa6
cortelucas <corte1994.lc@gmail.com>
2026-04-17 18:14:37 -0300

    feat(contracts): add IStreamStep contract

--- src/shared/contracts/IStep.ts
+++ src/shared/contracts/IStep.ts
@@ -1,3 +1,10 @@
 export interface IStep<TInput = void, TOutput = void> {
   execute(input: TInput): Promise<TOutput>;
 }
+
+export interface IStreamStep<TInput = void, TOutput = void> {
+  execute(
+    input: TInput,
+    onBatch: (batch: TOutput) => Promise<void>,
+  ): Promise<void>;
+}
