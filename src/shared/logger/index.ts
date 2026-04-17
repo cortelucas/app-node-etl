12178ceea37f1c5bcb3a33c7d49e724f15ed5f72
cortelucas <corte1994.lc@gmail.com>
2026-04-17 18:05:18 -0300

    feat(logger): add pino logger

---
+++ src/shared/logger/index.ts
@@ -0,0 +1,11 @@
+import pino from 'pino';
+
+export const logger = pino({
+  level: 'info',
+  formatters: {
+    level(label) {
+      return { level: label };
+    },
+  },
+  timestamp: pino.stdTimeFunctions.isoTime,
+});
